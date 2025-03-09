4934dc594bfbe20eb588e3e2f88774a85c9375d9
Ssiyam0123 <ssiyam563@gmail.com>
2025-03-10 05:13:21 +0600

    sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import * as Sentry from "@sentry/react";
 
+Sentry.init({
+  dsn: "https://56a4efba96aceb592b00c8c49c8307bf@o4508950249734144.ingest.us.sentry.io/4508950252552192",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+        useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
+    }),
+],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 const router = createBrowserRouter([
   {
     path: "/",
