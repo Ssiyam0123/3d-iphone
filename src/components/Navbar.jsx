2d797b738111aae091eaf9141cc20976a2c24430
Ssiyam0123 <ssiyam563@gmail.com>
2025-03-11 00:54:55 +0600

    live link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = () => {
 
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
-            <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
+            <a key={nav} href='https://www.facebook.com/siyam.ahmmed.528' className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
               {nav}
-            </div>
+            </a>
           ))}
         </div>
 
